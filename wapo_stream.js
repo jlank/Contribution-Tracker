3da819f046324bf5105eccc592c3ec075cda0465
John Lancaster <john.k.lancaster@gmail.com>
2012-10-06 11:54:55 -0400

    looping through sample json from transparencydata

--- wapo_stream.js
+++ wapo_stream.js
@@ -2,7 +2,13 @@
 
 var es = require('event-stream'),
     wapojson = require('./transcripts_wapo_510');
+    transparency = require('./transparencydata');
 
 wapojson.objects.forEach(function (trans) {
-  console.log(trans.resource_uri);
+  //console.log(trans.resource_uri);
+});
+
+console.log(transparency[0].contributor_name);
+transparency.forEach(function (data) {
+  console.log(data.amount);
 });
