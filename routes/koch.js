2e7c74f012584b5b04f054fc03c4737c4303695a
John Lancaster <john.k.lancaster@gmail.com>
2012-10-06 23:45:34 -0400

    mostly everything is working, indexes by speaker, org/person, & date

--- routes/koch.js
+++ routes/koch.js
@@ -5,9 +5,16 @@ var es = require('event-stream'),
 
 exports.amount = function (req, res) {
   var amount = [];
+  var callback = req.query.callback;
+
   koch.forEach(function (data) {
     amount.push(data.amount);
   });
-  res.send(amount);
-  console.log(amount);
+
+  if (!req.query.callback) {
+    res.send(amount);
+  }
+  else {
+    res.send(callback + '(' + JSON.stringify(amount) + ')');
+  }
 };
--- wapo_stream.js
+++ wapo_stream.js
@@ -5,10 +5,30 @@ var es = require('event-stream'),
     transparency = require('./transparencydata');
     koch = require('./koch');
 
+    var count = 0;
 wapojson.objects.forEach(function (trans) {
-  //console.log(trans.resource_uri);
+  if (trans.speakers.length > 0) {
+    var obj = {};
+    var speaker = "";
+    count++;
+    trans.speakers.forEach(function (speaker) {
+      if (speaker === '/politics/transcripts/api/v1/speaker/1/') {
+        obj.speaker = 'obama';
+      }
+      if (speaker === '/politics/transcripts/api/v1/speaker/2/') {
+        obj.speaker = 'romney';
+      }
+      obj.date = trans.date;
+      obj.transcript = trans.text;
+    });
+
+    if (obj.speaker === 'obama' || obj.speaker === 'romney') {
+      console.log(JSON.stringify(obj));
+    }
+  }
 });
 
+/*
 //console.log(transparency[0].contributor_name);
 var date = [];
 var amount = [];
@@ -19,3 +39,4 @@ koch.forEach(function (data) {
 
 console.log(date);
 console.log(amount);
+*/
