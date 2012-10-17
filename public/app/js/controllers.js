5a24a8d039bfefe5f9ff2bab29e81b1046482624
nathanmaton <nate.maton@gmail.com>
2012-10-17 15:09:40 -0400

    added procfile

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -10,9 +10,10 @@ function MyCtrl1($scope, $http, $route, $routeParams, $location) {
   var mention_data = {};
   var contribution_data = {};
   var charts = {};
-  var created = false;
+  //var created = false;
   var md = [];
   var cd = [];
+  var maxamount = 0;
 
   $scope.searchBtn = function () {
     if($scope.query) {
@@ -20,12 +21,13 @@ function MyCtrl1($scope, $http, $route, $routeParams, $location) {
     }
   }
 
+//when someone searches...
   if ($routeParams.query) {
     var run = function (candidate) {
      charts[candidate] = {};
      $scope.query = $routeParams.query;
      var host = 'jlank.wapohack.jit.su';
-     //host = 'localhost:3000';
+
      var mentionUrl = 'http://'+host+'/search2?query='+$routeParams.query+'&candidate='+candidate.toLowerCase()+'&callback=JSON_CALLBACK';
      //console.log(mentionUrl);
      $http.jsonp(mentionUrl)
@@ -43,32 +45,22 @@ function MyCtrl1($scope, $http, $route, $routeParams, $location) {
             charts['candidate'] = candidate;
             //console.log('creating mention charts');
             createChart(charts.candidate, charts[candidate].cd, charts[candidate].md);
-
-            //charts['candidate'] = candidate;
-            //console.log('creating mention charts');
-            //createChart(charts.candidate, cd, md);
           }
             md = [];
             cd = [];
-
-            /*
-          contribution_data = [];
-          data_date = [];
-          dedupe = {};
-          sorted_keys = [];
-          mention_data = [];
-          */
         });
 
+      //need to get API keys out of here
      var contribUrl = 'http://transparencydata.com/api/1.0/contributions.json?apikey=f266a32377604e7d91bbbafd76abb4cc&recipient_ft='+candidate+'&cycle=2012&contributor_ft=' + $routeParams.query + '&callback=JSON_CALLBACK';
 
-     //console.log(contribUrl);
+     //call transparency data for info
      $http.jsonp(contribUrl)
         .success(function (data) {
-            // loop through and put keys into a hash and add up values
+            //on successfull request loop through and put keys into a hash and add up values
             data.forEach(function(item){
               var amount = (parseInt(item.amount));
               var dda = item.date.split('-'); // dda = date_detail_array
+              
               if (!dedupe[item.date]) {
                 dedupe[item.date] = 0;
               }
@@ -83,34 +75,38 @@ function MyCtrl1($scope, $http, $route, $routeParams, $location) {
 
             sorted_keys.forEach(function (key) {
               var dda = key.split('-');
-              var ddy = parseInt(dda[0], 10); //date detail year
+              var ddy = parseInt(dda[0], 10); //date detail year, note the '10' param in parseInt for cross browser compatability
               var ddm = parseInt(dda[1], 10);
               var ddd = parseInt(dda[2], 10);
               var dda_int = [ddy,ddm,ddd];
-              cd.push([Date.UTC(ddy,ddm,ddd),dedupe[key]]);
+              if (dedupe[key]>0){
+                cd.push([Date.UTC(ddy,ddm,ddd),dedupe[key]]);
+              }
+
+              //find the max amount
+              if (dedupe[key]>maxamount){
+                maxamount = dedupe[key]
+              }
 
             });
+            console.log(maxamount)
+
             charts[candidate]['cd'] = cd;
             charts['candidate'] = candidate;
-            //console.log('creating charts');
-            //console.log(candidate);
-            createChart(charts.candidate, charts[candidate].cd, charts[candidate].md);
+            createChart(charts.candidate, charts[candidate].cd, charts[candidate].md, maxamount);
             md = [];
             cd = [];
-          /*
-          contribution_data = [];
-          data_date = [];
-          dedupe = {};
-          sorted_keys = [];
-          mention_data = [];
-          */
+            //return cd
+
         });
      window.chartss = charts;
 
   };
 
   run('Romney');
+  //console.log(run('Romney'))
   run('Obama');
+  //createChart(charts.Romney , charts[Romney].cd, charts[Romney].md, maxamount);
 
 }
 
